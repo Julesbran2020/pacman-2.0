f6c5e5d1b5fee0e74d684caf556d2c03da175bf9
Juliana Garcia <juliana.garcia0204@gmail.com>
2025-11-19 14:10:09 -0700

    Initializing app.java with Jframe and main Method

---
+++ app.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class app {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21 ;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+JFrame frame = new JFrame("Pac-Man");
+frame.setVisible(true);
+frame.setSize(boardWidth, boardHeight);
+frame.setLocationRelativeTo(null);
+frame.setResizable(false);
+frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
