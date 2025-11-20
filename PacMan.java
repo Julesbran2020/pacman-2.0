0378da187dc53282b626533ce37cb9ebef2a41d4
Juliana Garcia <juliana.garcia0204@gmail.com>
2025-11-19 17:32:42 -0700

    Implement tileMap loading and create Block sets

--- PacMan.java
+++ PacMan.java
@@ -4,17 +4,117 @@ import java.util.HashSet;
 import java.util.Random;
 import javax.swing.*;
 
+
 public class PacMan extends JPanel implements ActionListener, KeyListener {
        class Block {
-        PacMan() {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+        int startX;
+        int startY;
+
+        char direction = 'U';// U D L R
+        int velocityX = 0;
+        int velocityY = 0;
+
+        Block (Image image, int x, int  y,int width, int height) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
+    private int rowCount = 21 ;
+    private int columnCount = 19 ;
+    private int tileSize = 32;
+    private int boardWidth = columnCount * tileSize;
+    private int boardHeight = rowCount * tileSize;
+
+    //X = wall, O = skip, P = pac man, ' ' = food
+    //Ghosts: b = blue, o = orange, p = pink, r = red
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X                 X",
+        "X XX X XXXXX X XX X",
+        "X    X       X    X",
+        "XXXX XXXX XXXX XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXrXX X XXXX",
+        "O       bpo       O",
+        "XXXX X XXXXX X XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXXXX X XXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X  X     P     X  X",
+        "XX X X XXXXX X X XX",
+        "X    X   X   X    X",
+        "X XXXXXX X XXXXXX X",
+        "X                 X",
+        "XXXXXXXXXXXXXXXXXXX" 
+    };
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    Block pacman;
+    PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.black);
         addKeyListener(this);
         setFocusable(true);
-    } 
-}
-// Game Loop Logic
-     @Override
+    }
+    public void loadMap() {
+    walls = new HashSet<Block>();
+    foods = new HashSet<Block>();
+    ghosts = new HashSet<Block>();
+
+        for (int r = 0; r < rowCount; r++) {
+         String row = tileMap[r];            // get row once per r
+            for (int c = 0; c < columnCount; c++) { // use columnCount here
+                char tileMapChar = row.charAt(c);
+
+                int x = c * tileSize;
+                int y = r * tileSize;
+
+                if (tileMapChar == 'X') { //Block wall
+                Block wall = new Block(wallImage, x, y, tileSize, tileSize);
+                walls.add(wall); 
+                }
+                else if (tileMapChar == 'b'){ // Blue ghost
+                Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'o'){ // Orange ghost
+                Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'p'){ // Pink ghost
+                Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'r'){ // Red ghost
+                Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);   
+                }
+                else if (tileMapChar == 'P'){ // Pacman 
+                    pacman = new Block(pacmanRightImage , x, y, tileSize, tileSize);
+                }
+                else if (tileMapChar == ' '){ // Food
+                Block food = new Block(null, x + 14, y + 14, 4, 4);
+                foods.add(food);
+                }
+
+            }
+        }
+    }
+    @Override
     public void actionPerformed (ActionEvent e) {}
     @Override
     public void keyTyped(KeyEvent e) {}
