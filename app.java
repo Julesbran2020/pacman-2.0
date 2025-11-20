c5fdf15f816622721e0a0b11d5cbf6fafa7aef8a
Juliana Garcia <juliana.garcia0204@gmail.com>
2025-11-20 01:18:12 -0700

    Final Touches

--- app.java
+++ app.java
@@ -9,11 +9,18 @@ public class app {
         int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
-JFrame frame = new JFrame("Pac-Man");
-frame.setVisible(true);
-frame.setSize(boardWidth, boardHeight);
-frame.setLocationRelativeTo(null);
-frame.setResizable(false);
-frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Pac-Man");
+       // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+
     }
 }
